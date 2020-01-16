37a0842d03810d2101f568d6eae8c81b112b55a1
Ludvik Rønning <lro@shifthyperloop.com>
2020-01-16 10:35:23 +0100

    fungerende eksempel som detekterer grei andel av fulger

--- testCV.py
+++ testCV.py
@@ -3,15 +3,41 @@ import numpy as np
 
 # read image in greyscale
 
-img = cv2.imread('Penguins.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('Bilder/Birds.jpg', cv2.IMREAD_GRAYSCALE)
 img = cv2.resize(img, (650,500))
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 100
+params.maxThreshold = 255
+
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 20
+
+# Filter by Circularity
+params.filterByCircularity = False
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = False
+params.minConvexity = 0.87
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
 
 #determine cv version
 print(cv2.__version__)
 if cv2.__version__.startswith('2'):
-    detector = cv2.SimpleBlobDetector()
+    detector = cv2.SimpleBlobDetector(params)
 else:
-    detector = cv2.SimpleBlobDetector_create()
+    detector = cv2.SimpleBlobDetector_create(params)
+
 
 # detect suff
 keypoints = detector.detect(img)
